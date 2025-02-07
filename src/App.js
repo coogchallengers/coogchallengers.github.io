9dc7de5d12e5b8285bd98d351640be4371b31e2d
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2025-02-07 02:42:59 -0600

    added routes for leaderboard pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Events from './components/Events/Events'; // Import Events component
 import Header from './components/shared/Header';
 import Footer from './components/shared/Footer';
 
+import Leaderboard from './components/Leaderboard/Leaderboard';
+import OverallLeaderboard from './components/Leaderboard/OverallLeaderboard';
+import CompetitionLeaderboard from './components/Leaderboard/CompetitionLeaderboard';
+
 function App() {
   return (
     <Router>
@@ -14,6 +18,11 @@ function App() {
         <Route path="/events" element={<Events />} /> Render Events component
         
         {/* <Route path="/resources" element={<Resources />} /> */}
+
+        <Route path="/leaderboard/:category" element={<Leaderboard />} />
+        <Route path="/leaderboard" element={<OverallLeaderboard />} />
+        <Route path="/competition/:id" element={<CompetitionLeaderboard />} />
+        
       </Routes>
       <Footer />
     </Router>
